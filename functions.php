12a30873a7551bad4d63122c7d09211245ce34e7
skotperez <voraginebramante@gmail.com>
2014-11-10 11:45:26 +0100

    basic theme files

---
+++ functions.php
@@ -0,0 +1,125 @@
+<?php
+// theme setup main function
+add_action( 'after_setup_theme', 'hce_theme_setup' );
+function hce_theme_setup() {
+
+	// theme global vars
+	if (!defined('HCE_BLOGNAME'))
+	    define('HCE_BLOGNAME', get_bloginfo('name'));
+
+	if (!defined('HCE_BLOGDESC'))
+	    define('HCE_BLOGDESC', get_bloginfo('description','display'));
+
+	if (!defined('HCE_BLOGURL'))
+	    define('HCE_BLOGURL', get_bloginfo('url'));
+
+	if (!defined('HCE_BLOGTHEME'))
+	    define('HCE_BLOGTHEME', get_bloginfo('template_directory'));
+
+	/* Set up media options: sizes, featured images... */
+	add_action( 'init', 'hce_media_options' );
+
+	/* Add your nav menus function to the 'init' action hook. */
+	add_action( 'init', 'hce_register_menus' );
+
+	/* Load JavaScript files on the 'wp_enqueue_scripts' action hook. */
+	add_action( 'wp_enqueue_scripts', 'hce_load_scripts' );
+
+	// Custom post types
+	add_action( 'init', 'hce_create_post_type', 0 );
+
+	// Extra meta boxes in editor
+	//add_filter( 'cmb_meta_boxes', 'hce_metaboxes' );
+	// Initialize the metabox class
+	//add_action( 'init', 'hce_init_metaboxes', 9999 );
+
+	// excerpt support in pages
+	add_post_type_support( 'page', 'excerpt' );
+
+	// remove unused items from dashboard
+	add_action( 'admin_menu', 'hce_remove_dashboard_item' );
+
+	// disable admin bar in front end
+	add_filter('show_admin_bar', '__return_false');
+
+} // end hce theme setup function
+
+// set up media options
+function hce_media_options() {
+	/* Add theme support for post thumbnails (featured images). */
+	add_theme_support( 'post-thumbnails', array( 'project') );
+	set_post_thumbnail_size( 231, 0 ); // default Post Thumbnail dimensions
+	/* set up image sizes*/
+	update_option('thumbnail_size_w', 231);
+	update_option('thumbnail_size_h', 0);
+	update_option('medium_size_w', 474);
+	update_option('medium_size_h', 0);
+	update_option('large_size_w', 717);
+	update_option('large_size_h', 0);
+} // end set up media options
+
+// register custom menus
+function hce_register_menus() {
+        if ( function_exists( 'register_nav_menus' ) ) {
+                register_nav_menus(
+                array(
+                        'main-menu' => 'Menú principal',
+                        'secondary-menu' => 'Menú secundario',
+                )
+                );
+        }
+} // end register custom menus
+
+// load js scripts to avoid conflicts
+function hce_load_scripts() {
+	wp_enqueue_script('jquery');
+//	wp_enqueue_script(
+//		'bootstrap.min',
+//		get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js',
+//		array( 'jquery' ),
+//		'2.3.2',
+//		FALSE
+//	);
+
+} // end load js scripts to avoid conflicts
+
+// register post types
+function hce_create_post_type() {
+	// Documento custom post type
+	register_post_type( 'project', array(
+		'labels' => array(
+			'name' => __( 'Projects' ),
+			'singular_name' => __( 'Project' ),
+			'add_new_item' => __( 'Add a project' ),
+			'edit' => __( 'Edit' ),
+			'edit_item' => __( 'Edit this project' ),
+			'new_item' => __( 'New project' ),
+			'view' => __( 'View project' ),
+			'view_item' => __( 'View this project' ),
+			'search_items' => __( 'Search project' ),
+			'not_found' => __( 'No project found' ),
+			'not_found_in_trash' => __( 'No projects in trash' ),
+			'parent' => __( 'Parent' )
+		),
+		'has_archive' => true,
+		'public' => true,
+		'publicly_queryable' => true,
+		'exclude_from_search' => false,
+		'menu_position' => 5,
+		//'menu_icon' => get_template_directory_uri() . '/images/icon-post.type-integrantes.png',
+		'hierarchical' => false, // if true this post type will be as pages
+		'query_var' => true,
+		'supports' => array('title', 'editor','excerpt','author','thumbnail' ),
+		'rewrite' => array('slug'=>'project','with_front'=>false),
+		'can_export' => true,
+		'_builtin' => false,
+		'_edit_link' => 'post.php?post=%d',
+	));
+} // end register post types
+
+// remove item from wordpress dashboard
+function hce_remove_dashboard_item() {
+	remove_menu_page('edit.php');	
+}
+
+?>
---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+<title>
+<?php
+	/* From twentyeleven theme
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
+
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	echo HCE_BLOGNAME;
+
+	// Add the blog description for the home/front page.
+	if ( HCE_BLOGDESC && ( is_home() || is_front_page() ) )
+		echo " | " . HCE_BLOGDESC;
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | Página ' . max( $paged, $page );
+	?>
+</title>
+
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+
+<link rel="alternate" type="application/rss+xml" title="<?php echo HCE_BLOGNAME; ?> RSS Feed suscription" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="alternate" type="application/atom+xml" title="<?php echo HCE_BLOGNAME; ?> Atom Feed suscription" href="<?php bloginfo('atom_url'); ?>" /> 
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<?php
+if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
+wp_head(); ?>
+
+</head>
+
+<?php // better to use body tag as the main container ?>
+<body <?php body_class(); ?>>
