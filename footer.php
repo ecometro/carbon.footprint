37cdee8c263eb9d4af8c34efa16195d43b22a60f
skotperez <voraginebramante@gmail.com>
2014-11-27 14:05:41 +0100

    Parters' logos in footer

--- footer.php
+++ footer.php
@@ -1,5 +1,14 @@
 <footer id="epi" class="row">
 	<div class="epi-content col-md-12">
+		<ul class="list-inline">
+		<li class="text-muted"><strong>Una iniciativa de</strong><br /><a href="http://www.sostenibilidadyarquitectura.com/"><img src="<?php echo HCE_BLOGTHEME."/images/logo.asa.png" ?>" alt="Logo de la Asociación Sostenibilidad y Arquitectura" /></a></li>
+		<li class="text-muted"><strong>Con el apoyo de</strong><br /><a href="http://fundacion-biodiversidad.es/"><img src="<?php echo HCE_BLOGTHEME."/images/logo.f.biodiversidad.png" ?>" alt="Logo de la Fundación Biodiversidad" /></a></li>
+		<li class="text-muted"><strong>Y la colaboración de</strong><br />
+		<a href="http://www.ietcc.csic.es"><img src="<?php echo HCE_BLOGTHEME."/images/logo.i.torroja.png" ?>" alt="Logo del Instituto Eduardo Torroja" /></a>
+		<a href="http://www.magrama.gob.es/es/cambio-climatico/temas/organismos-e-instituciones-implicados-en-la-lucha-contra-el-cambio-climatico-a-nivel-nacional/oficina-espanola-en-cambio-climatico/"><img src="<?php echo HCE_BLOGTHEME."/images/logo.oecc.png" ?>" alt="Logo de la Oficina Española del Cambio Climático" /></a></li>
+		</ul>
+	</div>
+	<div class="col-md-12">
 	<?php $location = "footer-menu";
 	if ( has_nav_menu( $location ) ) {
 		$args = array(
