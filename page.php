84c1243f4e3d60da2a5e3bcbd95f9df2faf41c29
skotperez <voraginebramante@gmail.com>
2014-12-16 16:48:37 +0100

    default page template added

---
+++ page.php
@@ -0,0 +1,18 @@
+<?php get_header();
+
+if ( have_posts() ) { while ( have_posts() ) : the_post();
+?>
+
+<header class="row" role="banner">
+	<h1 class="col-md-12 page-header"><?php the_title(); ?></h1>
+</header>
+
+<main class="row" role="main">
+	<section class="col-md-12">
+		<?php the_content(); ?>
+	</section>
+</main>
+
+<?php endwhile;
+} // end if have_posts
+get_footer(); ?>
